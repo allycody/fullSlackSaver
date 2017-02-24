167dfcfcf9f51ab9f99f3bb00a59d4c158bdc918
Ian Munro <ianmunrobot@gmail.com>
2017-02-24 15:08:44 -0500

    updated user feedback

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,8 @@ router.get('/', function(req, res, next){
 })
 
 router.post('/email', function(req, res, next){
-	console.log('~~~~~in EMAIL ROUTE~~~~~~')
-	console.log(req.body)
-	let index = req.body.text.indexOf(' ');
+	let messageSeparatorIndex = req.body.text.indexOf(' ');
+	let messageSeparatorIndex = req.body.text.indexOf(' ');
 	if (index === -1) {
 		let error = new Error('Invalid syntax Usage: `/email <email> <message>')
 		next(error)
@@ -23,18 +22,16 @@ router.post('/email', function(req, res, next){
 				from: '<fullslacksaver@gmail.com>', // sender address
 				to: email, // list of receivers
 				subject: `Slack message from ${req.body.team_domain} ✔`, // Subject line
-				html: `<b>Saved from team ${req.body.team_domain} / ${req.body.channel_name}</b><br />${text}` // plaintext body
-				// html: '<b>Hello world ?</b>' // html body
+				html: `<b>Saved from team ${req.body.team_domain} / ${req.body.channel_name}</b><br />${text}` // html body
 		};
 
 		// send mail with defined transport object
 		transporter.sendMail(mailOptions, function(error, info){
 				if(error){
-						res.send(`Sending Failed: ${error.message}`)
-						return console.log(error);
+						next(new Error(`Sending Failed: ${error.message}`))
 				}
 				console.log('Message sent: ' + info.response);
-				res.send('Your message was sent')
+				res.send(`Your message was sent to ${email}`)
 		});
 	} else {
 		res.send('invalid email address\nUsage: `/email <email> <message>`')
